9123eb402c51aa3137a2fd732d0e941ceaf4a3aa
leoschet <leoschettini2@gmail.com>
2016-08-11 18:26:21 -0300

    basic node server

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser')
+
+var app = express();
+var port = 3700;
+
+app.use(bodyParser.json());
+
+// sets where template files are
+app.set('views', __dirname + '/templates');
+// sets where static files are (css, js...)
+app.use(express.static(__dirname + '/static'));
+
+// .html view setup - need to install ejs
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.listen(port);
+console.log("Listening on port " + port);
