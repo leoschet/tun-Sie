0ff8524b36c468f3ded7f8f01748e929816ccefd
leoschet <leoschettini2@gmail.com>
2016-08-18 04:22:56 -0300

    Update exemple certificate and adjust the Auto-wrapper variables

--- static/js/index.js
+++ static/js/index.js
@@ -32,7 +32,7 @@ function downloadSingle(format) {
 	var namesList = getNames(document.getElementById('names').value);
 	var totalDownloads = 0;
 	for (var i = namesList.length - 1; i >= 0; i--) {
-		var fileName = courseName + 'Certificate - ' + namesList[i];
+		var fileName = courseName + ' - ' + namesList[i];
 		console.log('nome: ' + namesList[i]);
 
 		// Replace the name without concerning the line size
@@ -85,18 +85,23 @@ function replaceName(newName, lastName = "{name}") {
 	textField.innerHTML = textField.innerHTML.replace(lastName, newName);
 }
 
-// For each 2 letters less than 45, take out 6.6 of x position. from -11.8 to 285. middle at 136.6
+// For each 2 letters less than 58, take out 6.6 of x position. from 0 to 412. middle at 136.6
 function adjustLines() {
 	var textField = document.getElementById('text-to-replace');
 	
 	var buffer = '';
-	var maxLength = 45;
-	var perLetter = 3.3;
-	var minimun = -11.8;
+	var maxLength = 58;
+	// Adjusted minimum (0 + 10) to get acceptable error area (different width for differents letters i and p)
+	// TODO: get the minimum dinamically, getting the rect size
+	var minimum = 10;
+	var maximum = 412;
+	var perLetter = (maximum-minimum)/maxLength;
 	for (var i = 0; i < textField.children.length; i++) {
 		// Get stored words and place in the beginning of the new line text
-		if (buffer.length != '') {
+		if (buffer.length != 0) {
+			reloadBlankSpace(buffer);
 			textField.children[i].innerHTML = buffer + textField.children[i].innerHTML;
+			textField.children[i].innerHTML.trim();
 			buffer = '';
 		}
 
@@ -106,15 +111,18 @@ function adjustLines() {
 		// The text fits the max length
 		if (lengthVariation >= 0) {
 			// Adjust text's x position
-			textField.children[i].setAttribute('x', getXPosition(lengthVariation, minimun, perLetter))
+			textField.children[i].setAttribute('x', getXPosition(lengthVariation, minimum, perLetter));
 		} else {
 			// If the text don't fit, the code need to guarantee two conditions:
-			// - The textLength are equal or less than 45
+			// - The textLength are equal or less than maxLength
 			// - No word will be wrapped
+			// - The blankspace will not be removed
 
+			// The last considered char, for this line, must be at position 'maxLength'
+			var index = maxLength;
+			
 			// By removing all chars after the maxLength, the code can guarantee the textLength condition
 			// By searching for the first blank space before the maxLength, the code guarantee the wrap condition
-			var index = maxLength;
 			while(textField.children[i].innerHTML[index] != ' '){
 				index -= 1;
 				if (index == -1) {
@@ -123,20 +131,27 @@ function adjustLines() {
 				}
 			}
 
-			// Store the text that will be removed (without the blank space) on the buffer
-			buffer = textField.children[i].innerHTML.substring(index+1, textField.children[i].innerHTML.length);
+			// Store the text that will be removed (with the blank space) on the buffer
+			buffer = textField.children[i].innerHTML.substring(index, textField.children[i].innerHTML.length);
+			buffer = buffer + ' ';
 
-			// Remove the text
+			// Remove the text (whitout the blank space)
 			textField.children[i].innerHTML = textField.children[i].innerHTML.substring(0, index);
 
 			// Adjust the text's x position
-			var newLengthVariation = maxLength - textField.children[i].innerHTML.length;
-			textField.children[i].setAttribute('x', getXPosition(newLengthVariation, minimun, perLetter))
+			lengthVariation = maxLength - textField.children[i].innerHTML.length;
+			textField.children[i].setAttribute('x', getXPosition(lengthVariation, minimum, perLetter));
 		}
 
 	}
 }
 
-function getXPosition(lengthVariation, minimun, perLetter) {
-	return (lengthVariation * perLetter) + minimun;
+function reloadBlankSpace(str) {
+	str.trim();
+	str = str + ' ';
+}
+
+function getXPosition(lengthVariation, minimum, perLetter) {
+	var oneSideLengthVariation = lengthVariation/2
+	return (oneSideLengthVariation * perLetter) + minimum;
 }
